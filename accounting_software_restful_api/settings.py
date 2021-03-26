8ac5f48727814c6d936668ab90ee1cb0b74cb504
loloman <kirilldd2@gmail.com>
2021-03-26 21:15:30 +0700

    production fix;

--- accounting_software_restful_api/settings.py
+++ accounting_software_restful_api/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from . import production_settings
+except ImportError:
+    pass
