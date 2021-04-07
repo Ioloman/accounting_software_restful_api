6ac7f24579ade48e8541c3b4bfb645c3058383e7
loloman <kirilldd2@gmail.com>
2021-04-07 22:54:35 +0700

    made server side doc's lines saving;

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -36,6 +36,62 @@ class ReportSerializer(serializers.ModelSerializer):
     report_lines = ReportLineSerializer(read_only=True, many=True, allow_null=True, source='reportline_set', required=False)
     url = serializers.HyperlinkedIdentityField(view_name='api:report-detail')
 
+    def update(self, instance: Report, validated_data):
+        instance.doc_num = validated_data.get('doc_num', instance.doc_num)
+        instance.date = validated_data.get('date', instance.date)
+        instance.workshop_sender_pk = validated_data.get('workshop_sender_pk', instance.workshop_sender_pk)
+        instance.save()
+
+        new_lines = self.initial_data.get('report_lines', [])
+        old_lines = instance.reportline_set.all()
+        old_lines = [dict(ReportLineSerializer(instance=line, context={'request': self.context['request']}).data) for line in old_lines]
+
+        to_delete = []
+        to_change = []
+        to_create = []
+
+        for old_line in old_lines:
+            for new_line in new_lines:
+                if old_line['report_line_pk'] == new_line['report_line_pk']:
+                    break
+            else:
+                to_delete.append(old_line)
+        for delete in to_delete:
+            old_lines.remove(delete)
+
+        for new_line in new_lines:
+            for old_line in old_lines:
+                if old_line['report_line_pk'] == new_line['report_line_pk']:
+                    break
+            else:
+                to_create.append(new_line)
+        for create in to_create:
+            new_lines.remove(create)
+
+        for new_line in new_lines:
+            for old_line in old_lines:
+                if old_line['report_line_pk'] == new_line['report_line_pk']:
+                    if old_line == new_line:
+                        break
+            else:
+                to_change.append(new_line)
+
+        for delete in to_delete:
+            ReportLine.objects.get(report_line_pk=delete['report_line_pk']).delete()
+
+        for create in to_create:
+            serialized_line = ReportLineSerializer(data=create)
+            if serialized_line.is_valid():
+                serialized_line.save()
+
+        for change in to_change:
+            line: ReportLine = ReportLine.objects.get(report_line_pk=change['report_line_pk'])
+            serialized_line = ReportLineSerializer(line, data=change)
+            if serialized_line.is_valid():
+                serialized_line.save()
+
+        return instance
+
     class Meta:
         model = Report
         fields = ['url', 'report_pk', 'doc_num', 'date', 'workshop_sender_pk', 'report_lines']
@@ -58,6 +114,62 @@ class VedomostLineSerializer(serializers.ModelSerializer):
 class VedomostSerializer(serializers.ModelSerializer):
     vedomost_lines = VedomostLineSerializer(read_only=True, many=True, allow_null=True, source='vedomostline_set', required=False)
     url = serializers.HyperlinkedIdentityField(view_name='api:vedomost-detail')
+
+    def update(self, instance: Vedomost, validated_data):
+        instance.doc_num = validated_data.get('doc_num', instance.doc_num)
+        instance.creation_date = validated_data.get('creation_date', instance.creation_date)
+        instance.workshop_pk = validated_data.get('workshop_pk', instance.workshop_pk)
+        instance.save()
+
+        new_lines = self.initial_data.get('vedomost_lines', [])
+        old_lines = instance.vedomostline_set.all()
+        old_lines = [dict(VedomostLineSerializer(instance=line, context={'request': self.context['request']}).data) for line in old_lines]
+
+        to_delete = []
+        to_change = []
+        to_create = []
+
+        for old_line in old_lines:
+            for new_line in new_lines:
+                if old_line['vedomost_line_pk'] == new_line['vedomost_line_pk']:
+                    break
+            else:
+                to_delete.append(old_line)
+        for delete in to_delete:
+            old_lines.remove(delete)
+
+        for new_line in new_lines:
+            for old_line in old_lines:
+                if old_line['vedomost_line_pk'] == new_line['vedomost_line_pk']:
+                    break
+            else:
+                to_create.append(new_line)
+        for create in to_create:
+            new_lines.remove(create)
+
+        for new_line in new_lines:
+            for old_line in old_lines:
+                if old_line['vedomost_line_pk'] == new_line['vedomost_line_pk']:
+                    if old_line == new_line:
+                        break
+            else:
+                to_change.append(new_line)
+
+        for delete in to_delete:
+            VedomostLine.objects.get(vedomost_line_pk=delete['vedomost_line_pk']).delete()
+
+        for create in to_create:
+            serialized_line = VedomostLineSerializer(data=create)
+            if serialized_line.is_valid():
+                serialized_line.save()
+
+        for change in to_change:
+            line: VedomostLine = VedomostLine.objects.get(vedomost_line_pk=change['vedomost_line_pk'])
+            serialized_line = VedomostLineSerializer(line, data=change)
+            if serialized_line.is_valid():
+                serialized_line.save()
+
+        return instance
 
     class Meta:
         model = Vedomost
